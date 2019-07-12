476c4640cceeffa6ecfee11dd9906021ff23e826
Marina Sakovich <sakovich.mandarina@gmail.com>
2019-07-12 15:39:46 +0300

    Add migration infrastructure

---
+++ src/migrations/migration.service.js
@@ -0,0 +1,53 @@
+const db = require('db');
+const fs = require('fs');
+const path = require('path');
+const schema = require('./migration.schema');
+
+const service = db.createService('__migrationVersion', schema);
+const migrationsPath = path.join(__dirname, 'migrations');
+const _id = 'migration_version';
+
+const getMigrationNames = () => new Promise((resolve, reject) => {
+  fs.readdir(migrationsPath, (err, files) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(files);
+  });
+});
+
+service.getCurrentMigrationVersion = () => service.findOne({ _id })
+  .then((doc) => {
+    if (!doc) {
+      return 0;
+    }
+
+    return doc.version;
+  });
+
+service.getMigrations = () => {
+  let migrations = null;
+
+  return getMigrationNames().then((names) => {
+    migrations = names.map((name) => {
+      const migrationPath = path.join(migrationsPath, name);
+      return require(migrationPath); // eslint-disable-line
+    });
+
+    return migrations;
+  }).catch((err) => {
+    throw err;
+  });
+};
+
+service.setNewMigrationVersion = version => service.findOneAndUpdate({ _id }, {
+  $set: {
+    version,
+  },
+  $setOnInsert: {
+    _id,
+  },
+}, { upsert: true });
+
+module.exports = service;
---
+++ src/migrations/migrations-log/migration-log.service.js
@@ -0,0 +1,36 @@
+const db = require('db');
+const schema = require('./migration-log.schema.js');
+
+const service = db.createService('__migrationLog', schema);
+
+service.startMigrationLog = (_id, startTime, migrationVersion) => {
+  return service.findOneAndUpdate({ _id }, {
+    $set: {
+      migrationVersion,
+      startTime,
+      status: 'running',
+    },
+    $setOnInsert: {
+      _id,
+    },
+  }, { upsert: true });
+};
+
+service.failMigrationLog = (_id, finishTime, err) => service.atomic.update({ _id }, {
+  $set: {
+    finishTime,
+    status: 'failed',
+    error: err.message,
+    errorStack: err.stack,
+  },
+});
+
+service.finishMigrationLog = (_id, finishTime, duration) => service.atomic.update({ _id }, {
+  $set: {
+    finishTime,
+    status: 'completed',
+    duration,
+  },
+});
+
+module.exports = service;
