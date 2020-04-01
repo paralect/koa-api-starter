f14e696070801e01e6f0d7f4a6163f4bb8df0f2e
Michael Slizenko <m.slizenko@protonmail.com>
2020-04-01 21:31:36 +0300

    Add auto replica initialization in development

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
 docker-compose up -d --build
+
+sleep 2
+
+docker exec ship-mongo mongo --eval "
+rs.initiate(
+  {
+    _id : 'ship-rs',
+    members: [
+      { _id : 0, host : \"mongo:27017\" }
+    ]
+  }
+)
+"
