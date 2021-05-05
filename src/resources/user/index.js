371222055a7db8e3e54195dc942aa6250fb9248e
Emil Kuluev <Emilcul13@gmail.com>
2021-05-05 18:48:30 +0300

    add users route with pagiantion

--- src/resources/user/index.js
+++ src/resources/user/index.js
@@ -6,5 +6,6 @@ const router = new Router();
 
 require('./get-current').register(router);
 require('./update-current').register(router);
+require('./get-user').register(router);
 
 module.exports = router.routes();
