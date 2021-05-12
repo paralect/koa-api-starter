0321bc0b9e4be64eb4f3d90285707990d8c53723
Emil Kuluev <Emilcul13@gmail.com>
2021-05-12 11:24:01 +0300

    add watcher, it tracks changes in package.json file and restart if it does

---
+++ packageJsonWatcher.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+// eslint-disable-next-line
+const { Spinner } = require('cli-spinner');
+const moment = require('moment');
+
+(() => {
+  const spinner = new Spinner('processing.. %s');
+  spinner.setSpinnerString('|/-\\');
+
+  console.log('Start watching package.json');
+
+  fs.watchFile('package.json', () => {
+    console.log('file Changed');
+    spinner.start();
+
+    exec('npm install', (err, stdout) => {
+      const timestamp = moment().format();
+      if (err) {
+        console.error(err);
+        spinner.stop();
+      } else {
+        console.log(`stdout: ${stdout}`);
+        fs.writeFile('./src/tmp.js', timestamp, (error) => {
+          if (error) {
+            console.log(error);
+          }
+          spinner.stop();
+        });
+      }
+    });
+  });
+})();
